e761cce28466ec15e699b27d3c1bc0da39c6be4c
Alfonso Domínguez García <55954910+AlfonDG@users.noreply.github.com>
2023-04-23 02:57:26 +0200

    Add files via upload

---
+++ UF2/principal.py
@@ -0,0 +1,6 @@
+#Ejercicio Principal referente al paquete "recursivitat" que punta directamente al módulo con las funciones recursivas
+from recursivitat import recursivitat
+
+#Aquí printamos el módulo de recursividad y ejecutará todos los ejercicios del módulo.
+#En este caso todas las funciones del módulo recursivitat.
+print(recursivitat)
